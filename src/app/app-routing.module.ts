243f60ae55270ec8c3863246807ab0eff7defe1d
GummadiRavi <gummadi.ravi@gmail.com>
2020-05-03 16:54:20 +0530

    adding angular material and pwa support
    adding angular material and pwa support

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {DahsboardComponent} from "./views/dahsboard/dahsboard.component";
+import {TableComponent} from "./views/table/table.component";
+import {DragDropComponent} from "./views/drag-drop/drag-drop.component";
+import {PagenotfoundComponent} from "./views/pagenotfound/pagenotfound.component";
+import {AddressComponent} from "./views/address/address.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'dashboard', component: DahsboardComponent },
+  { path: 'data', component: TableComponent },
+  { path: 'drag-drop', component: DragDropComponent },
+  { path: 'address', component: AddressComponent },
+  { path: '',   redirectTo: '/dashboard', pathMatch: 'full' }, // redirect to `first-component`
+  { path: '**', component: PagenotfoundComponent },  // Wildcard route for a 404 page
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,73 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule} from "@angular/material/slider";
+import { FooterComponent } from './footer/footer/footer.component';
+import { AddressComponent } from './views/address/address.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCardModule } from '@angular/material/card';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NavigationComponent } from './header/navigation/navigation.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { DahsboardComponent } from './views/dahsboard/dahsboard.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatMenuModule } from '@angular/material/menu';
+import { TableComponent } from './views/table/table.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { TreeComponent } from './views/tree/tree.component';
+import { MatTreeModule } from '@angular/material/tree';
+import { DragDropComponent } from './views/drag-drop/drag-drop.component';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { PagenotfoundComponent } from './views/pagenotfound/pagenotfound.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    AddressComponent,
+    NavigationComponent,
+    DahsboardComponent,
+    TableComponent,
+    TreeComponent,
+    DragDropComponent,
+    PagenotfoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatCardModule,
+    ReactiveFormsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatGridListModule,
+    MatMenuModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatTreeModule,
+    DragDropModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
